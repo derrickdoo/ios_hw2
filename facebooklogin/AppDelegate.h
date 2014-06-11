d88507c9e2bed5dadaa272c30acf1cd92b1a41a1
derricko <derricko@yahoo-inc.com>
2014-06-10 17:31:23 -0700

    Initial Commit

---
+++ facebooklogin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  facebooklogin
+//
+//  Created by Derrick Or on 6/10/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
